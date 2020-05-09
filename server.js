8295a939896a13fb6c574111445c3d20d42a004d
rickvian <rickvianaldi@gmail.com>
2020-05-09 18:05:39 +0700

    attempt3

--- server.js
+++ server.js
@@ -39,5 +39,8 @@ app.use('/api',routes);
 if (process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
 }
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
