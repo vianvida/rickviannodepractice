e723e8fbb8691a4182a17e829d97893ea5f6365e
rickvian <rickvianaldi@gmail.com>
2020-05-09 18:10:49 +0700

    added path and try it

--- server.js
+++ server.js
@@ -39,6 +39,8 @@ app.use('/api',routes);
 if (process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
 }
+
+const path = require('path');
 app.get('*',(req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
