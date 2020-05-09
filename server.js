9e4fa3f53ef509913e24089bdfd61966f5f2931d
rickvian <rickvianaldi@gmail.com>
2020-05-09 17:55:49 +0700

    trying to fix error route

--- server.js
+++ server.js
@@ -40,4 +40,9 @@ if (process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
 }
 
+const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
